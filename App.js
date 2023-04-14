10a3cdcd05b072ef6c0013423d695f04c47fc5fb
Nico <nico_cartellone@hotmail.com>
2023-04-14 18:41:30 -0300

    test: fetch api dolar and show results

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native'
 
 export default function App () {
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState()
+  const [response, setResponse] = useState()
+  useEffect(() => {
+    fetch('https://api-dolar-argentina-nu.vercel.app/dolar')
+      .then(res => res.json())
+      .then((result) => {
+        setIsLoading(false)
+        setResponse(result)
+      },
+      (error) => {
+        setIsLoading(false)
+        setError(error)
+      }
+      )
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style='auto' />
+      <Text>Dolar App</Text>
+      {response === undefined
+        ? (
+          <View>
+            <ActivityIndicator color='blue' size={30} />
+            <Text>Cargando datos...</Text>
+          </View>
+          )
+        : (
+          <View>
+            <Text>Dolar Blue</Text>
+            <Text>{response.blue.compra}</Text>
+            <Text>{response.blue.venta}</Text>
+          </View>
+          )}
     </View>
   )
 }
