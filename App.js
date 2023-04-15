09d7420365ddf4222184bb0d48114be67d8d92ea
Nico <nico_cartellone@hotmail.com>
2023-04-15 17:11:06 -0300

    add core functionality

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true)
@@ -21,19 +21,10 @@ export default function App() {
       )
   }, [])
 
-  if(isLoading){
-    return (
-      <View>
-        <ActivityIndicator color="blue" size={30}/>
-        <Text>Cargando datos...</Text>
-      </View>
-    )
-  }
-
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Text>Dolar App</Text>
+      <Text style={styles.title}>Dólar App</Text>
       {response === undefined 
         ?
         <View>
@@ -41,10 +32,41 @@ export default function App() {
           <Text>Cargando datos...</Text>
         </View>
         :
-        <View>
-          <Text>Dolar Blue</Text>
-          <Text>{response.blue.compra}</Text>
-          <Text>{response.blue.venta}</Text>
+        <View style={styles.dolarContainer}>
+          <View style={styles.dolarBlueContainer}>
+            <Text style={styles.dolarTitle}>Dólar Blue</Text>
+            <Text style={styles.dolarText}>Compra {response.blue.compra}</Text>
+            <Text style={styles.dolarText}>Venta {response.blue.venta}</Text>
+          </View>
+
+          <View style={styles.dolarBlueContainer}>
+            <Text style={styles.dolarTitle}>Dólar Oficial</Text>
+            <Text style={styles.dolarText}>Compra {response.oficial.compra}</Text>
+            <Text style={styles.dolarText}>Venta {response.oficial.venta}</Text>
+          </View>
+
+          <View style={styles.dolarBlueContainer}>
+            <Text style={styles.dolarTitle}>Dólar Bolsa</Text>
+            <Text style={styles.dolarText}>Compra {response.bolsa.compra}</Text>
+            <Text style={styles.dolarText}>Venta {response.bolsa.venta}</Text>
+          </View>
+
+          <View style={styles.dolarBlueContainer}>
+            <Text style={styles.dolarTitle}>Dólar Contado con Liqui</Text>
+            <Text style={styles.dolarText}>Compra {response.blue.compra}</Text>
+            <Text style={styles.dolarText}>Venta {response.blue.venta}</Text>
+          </View>
+
+          <View style={styles.dolarBlueContainer}>
+            <Text style={styles.dolarTitle}>Dólar Cripto</Text>
+            <Text style={styles.dolarText}>Compra {response.blue.compra}</Text>
+            <Text style={styles.dolarText}>Venta {response.blue.venta}</Text>
+          </View>
+
+          <View style={styles.dolarBlueContainer}>
+            <Text style={styles.dolarTitle}>Dólar Solidario</Text>
+            <Text style={styles.dolarText}>Compra {response.blue.compra}</Text>
+          </View>
         </View>
       }
     </View>
@@ -56,6 +78,46 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FAFBFF"
   },
+  title: {
+    fontSize: 60,
+    marginVertical: 20,
+    paddingLeft: "5%",
+    paddingTop: 20
+  },
+  dolarContainer: {
+    flex: 1,
+    width: "90%",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  dolarTitle: {
+    fontSize: 30
+  },
+  dolarText:{
+    fontSize: 20
+  },
+  dolarBlueContainer: {
+    width: "90%",
+    marginVertical: 10,
+    justifyContent: "center",
+    alignItems: "flex-start",
+    paddingLeft: 20,
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 3,
+    borderTopColor: "green",
+    borderColor: "#FAFBFF",
+    // Shadow for iOS
+    shadowOpacity: 0.08,
+    shadowOffset: {
+      width: 0,
+      height: 20,
+    },
+    shadowRadius: 10,
+    // Shadow for Android
+    elevation: 5,
+  }
 });
