a761efdc0a382ff4266ec4b714096fe63416bfb2
Nico <nico_cartellone@hotmail.com>
2023-04-30 13:40:33 -0300

    fix: fech data. add: flatlist and dark mode

--- App.js
+++ App.js
@@ -1,90 +1,83 @@
+import { useEffect, useState, useCallback } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator, SafeAreaView, FlatList, useColorScheme } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(true)
-  const [error, setError] = useState()
-  const [response, setResponse] = useState()
+  const [response, setResponse] = useState([])
+  const colorScheme = useColorScheme();
+
+  const titleStyle = colorScheme === 'dark' ? styles.titleDark : styles.titleLight;
+  const textStyle = colorScheme === 'dark' ? styles.dolarTextDark : styles.dolarTextLight;
+  const titleDolarStyle = colorScheme === 'dark' ? styles.dolarTitleDark : styles.dolarTitleLight;
+  const dolarBlueContainerStyle = colorScheme === 'dark' ? styles.dolarBlueContainerDark : styles.dolarBlueContainerLight;
+  const containerStyle = colorScheme === 'dark' ? styles.containerDark : styles.containerLight;
+  const textLoadingStyle = colorScheme === 'dark' ? styles.textLoadingDark : styles.textLoadingLight
+
+  const fetchData = useCallback(async () => {
+    try {
+      const res = await fetch("https://api-dolar-argentina-nu.vercel.app/dolar");
+      const result = await res.json();
+      setResponse(result);
+    } catch (error) {
+      console.log(error)
+    }
+  }, []);
 
   useEffect(() => {
-    fetch("https://api-dolar-argentina-nu.vercel.app/dolar")
-      .then(res => res.json())
-      .then((result) => {
-        setIsLoading(false)
-        setResponse(result)
-      },
-        (error) => {
-          setIsLoading(false)
-          setError(error)
-        }
-      )
-  }, [])
-
+    fetchData();
+  }, [fetchData]);
+  
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={[styles.container, containerStyle]}>
       <StatusBar style="auto" />
-      <Text style={styles.title}>Dólar App</Text>
-      {response === undefined 
+      <Text style={[styles.title, titleStyle]}>Dólar App</Text>
+      {response.length === 0
         ?
-        <View>
-          <ActivityIndicator color="blue" size={30}/>
-          <Text>Cargando datos...</Text>
+        <View style={{flex:1, justifyContent: "center", alignContent: "center"}}>
+          <ActivityIndicator color="#079A7D" size={30}/>
+          <Text style={[styles.textLoading, textLoadingStyle]}>Cargando datos...</Text>
         </View>
         :
-        <View style={styles.dolarContainer}>
-          <View style={styles.dolarBlueContainer}>
-            <Text style={styles.dolarTitle}>Dólar Blue</Text>
-            <Text style={styles.dolarText}>Compra {response.blue.compra}</Text>
-            <Text style={styles.dolarText}>Venta {response.blue.venta}</Text>
-          </View>
-
-          <View style={styles.dolarBlueContainer}>
-            <Text style={styles.dolarTitle}>Dólar Oficial</Text>
-            <Text style={styles.dolarText}>Compra {response.oficial.compra}</Text>
-            <Text style={styles.dolarText}>Venta {response.oficial.venta}</Text>
-          </View>
-
-          <View style={styles.dolarBlueContainer}>
-            <Text style={styles.dolarTitle}>Dólar Bolsa</Text>
-            <Text style={styles.dolarText}>Compra {response.bolsa.compra}</Text>
-            <Text style={styles.dolarText}>Venta {response.bolsa.venta}</Text>
-          </View>
-
-          <View style={styles.dolarBlueContainer}>
-            <Text style={styles.dolarTitle}>Dólar Contado con Liqui</Text>
-            <Text style={styles.dolarText}>Compra {response.blue.compra}</Text>
-            <Text style={styles.dolarText}>Venta {response.blue.venta}</Text>
-          </View>
-
-          <View style={styles.dolarBlueContainer}>
-            <Text style={styles.dolarTitle}>Dólar Cripto</Text>
-            <Text style={styles.dolarText}>Compra {response.blue.compra}</Text>
-            <Text style={styles.dolarText}>Venta {response.blue.venta}</Text>
-          </View>
-
-          <View style={styles.dolarBlueContainer}>
-            <Text style={styles.dolarTitle}>Dólar Solidario</Text>
-            <Text style={styles.dolarText}>Compra {response.blue.compra}</Text>
-          </View>
+        <View style={{width: "80%"}}>
+          <FlatList
+            data={response}
+            renderItem={({item}) => (
+              <View style={[styles.dolarBlueContainer, dolarBlueContainerStyle]}>
+                <Text style={[styles.dolarTitle, titleDolarStyle]}>{item.tipo}</Text>
+                <Text style={[styles.dolarText, textStyle]}>Compra {item.compra}</Text>
+                <Text style={[styles.dolarText, textStyle]}>Venta {item.venta}</Text>
+              </View>
+            )}
+            keyExtractor={(item) => item.id}
+          />
         </View>
       }
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    backgroundColor: "#FAFBFF"
+  },
+  containerDark: {
+    backgroundColor: '#000000',
+  },
+  containerLight: {
+    backgroundColor: '#fff'
   },
   title: {
     fontSize: 60,
     marginVertical: 20,
     paddingLeft: "5%",
-    paddingTop: 20
+    paddingTop: 20,
+  },
+  titleLight: {
+    color: "black"
+  },
+  titleDark: {
+    color: "white"
   },
   dolarContainer: {
     flex: 1,
@@ -93,23 +86,36 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   dolarTitle: {
-    fontSize: 30
+    fontSize: 30,
+    
+    textTransform: "capitalize"
+  },
+  dolarTitleDark: {
+    color:"white",
+  },
+  dolarTitleLight: {
+    color:"black",
   },
   dolarText:{
-    fontSize: 20
+    fontSize: 20,
+  },
+  dolarTextDark:{
+    color: "white"
+  },
+  dolarTextLight:{
+    color: "black"
   },
   dolarBlueContainer: {
-    width: "90%",
     marginVertical: 10,
     justifyContent: "center",
     alignItems: "flex-start",
     paddingLeft: 20,
-    backgroundColor: "white",
+    backgroundColor: "#111111",
     padding: 10,
     borderRadius: 10,
     borderWidth: 3,
-    borderTopColor: "green",
-    borderColor: "#FAFBFF",
+    borderTopColor: "#079A7D",
+    borderColor: "#111111",
     // Shadow for iOS
     shadowOpacity: 0.08,
     shadowOffset: {
@@ -119,5 +125,22 @@ const styles = StyleSheet.create({
     shadowRadius: 10,
     // Shadow for Android
     elevation: 5,
+  },
+  dolarBlueContainerDark: {
+    backgroundColor: "#111111",
+    borderColor: "#111111",
+  },
+  dolarBlueContainerLight:{
+    backgroundColor: "white",
+    borderColor: "#FAFBFF",
+  },
+  textLoading:{
+    marginTop: 10
+  },
+  textLoadingDark: {
+    color: 'white'
+  },
+  textLoadingLight: {
+    color: 'black'
   }
 });
